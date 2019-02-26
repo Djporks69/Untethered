781ac21f22b6e85b5123d8ca1c796b96ee8acda0
Djporks69 <Djporks69@gmail.com>
2019-02-26 22:06:41 +0000

    Added RCS thruster sound effects and adjusted movement.
    Created an RCS thruster sound effect using a deodorant can. Also fixed some of the movement so the player will not fly towards or away from the screen.

--- Assets/Astronaut.cs
+++ Assets/Astronaut.cs
@@ -6,21 +6,34 @@ using UnityEngine;
 public class Astronaut : MonoBehaviour
 {
     Rigidbody rigidbody;
+    AudioSource audiosource;
 
+    bool soundon;
 
 
    
-
-    // Start is called before the first frame update
+    // Start is called before the first frame updatew
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>(); //Only act on components of type "Rigidbody"
+        audiosource = GetComponent<AudioSource>(); //Only act on components of type "AudioSource"
     }
 
-    // Update is called once per frame
+    // Update is called once per frame 
     void Update()
     {
         ProcessInput();
+        if (!audiosource.isPlaying)
+        {
+            if (soundon)
+            {
+                audiosource.Play();
+            }
+        }
+        if (!soundon)
+        {
+            audiosource.Stop();
+        }
     }
 
     void ProcessInput() // Allows player to fly upwards and rotate.
@@ -28,6 +41,11 @@ public class Astronaut : MonoBehaviour
         if (Input.GetKey(KeyCode.W)) // Can Thrust While rotating
         {
             rigidbody.AddRelativeForce(Vector3.up); // Makes the player fly upwards
+            soundon = true;
+        }
+        else
+        {
+            soundon = false;
         }
         if (Input.GetKey(KeyCode.D))// Can't Rotate Left at same time.
         {
